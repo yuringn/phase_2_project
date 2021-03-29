5e2ec433542f52154c91cc1d59c7a0d59c8ef3f0
Yuri Nguyen <yuringuyen312@gmail.com>
2021-03-29 17:03:27 -0400

    first commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Shelter.destroy_all
+Person.destroy_all
+Puppy.destroy_all
+Rental.destroy_all
+Advertisement.destroy_all
+
+Shelter.reset_pk_sequence
+Person.reset_pk_sequence
+Puppy.reset_pk_sequence
+Rental.reset_pk_sequence
+Advertisement.reset_pk_sequence
+
+10.times do 
+    Shelter.create(name: Faker::Movies::HarryPotter.spell, location: Faker::Movies::HarryPotter.location)
+end
+
+# Faker::Internet.username #=> "alexie"
+15.times do
+    Person.create(name: Faker::Movies::StarWars.character, age: rand(16..70), gender: Faker::Gender.binary_type, username: Faker::Internet.username, email: Faker::Internet.free_email)
+end
+
+#image !!!
+20.times do
+    Puppy.create(name: Faker::Creature::Dog.name, breed: Faker::Creature::Dog.breed, age: rand(1..10), cute_fact: Faker::Creature::Dog.meme_phrase, cost: rand(50..100), shelter_id: Shelter.all.sample.id)
+end
+
+20.times do
+    Rental.create(cost: rand(10..20), time: rand(1..24), puppy_id: Puppy.all.sample.id, person_id: Person.all.sample.id)
+end
+
+20.times do
+    Advertisement.create(shelter_id: Shelter.all.sample.id, puppy_id: Puppy.all.sample.id, info: Faker::Movies::PrincessBride.quote)
+end
+
+puts "Seeded"
+
