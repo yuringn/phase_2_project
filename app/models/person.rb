a69299922302162608c32074536a5b9d77244bee
Yuri Nguyen <yuringuyen312@gmail.com>
2021-03-30 16:14:06 -0400

    new signup form & a bit styling

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,8 @@
 class Person < ApplicationRecord
     has_many :rentals
     has_many :puppies
+
+    validates :username, :email, :age, presence: true
+
+    validates :username, :email, uniqueness: true
 end
